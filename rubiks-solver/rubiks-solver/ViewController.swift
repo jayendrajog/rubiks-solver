5eef6f9e26018763718cdd02d4d803dfee1527bc
Jayendra <jay08jog@gmail.com>
2015-09-12 16:42:47 -0700

    Initial Commit

---
+++ rubiks-solver/rubiks-solver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rubiks-solver
+//
+//  Created by Jayendra Ashutosh Jog on 9/12/15.
+//  Copyright (c) 2015 SCove Productions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
